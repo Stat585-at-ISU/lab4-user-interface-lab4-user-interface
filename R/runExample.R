607fb02c949cd9bdb8bf2535b2aa118c47b4c37d
hardtme <hardtme@iastate.edu>
2023-04-13 15:12:12 -0500

    Change app name in runExample

--- R/runExample.R
+++ R/runExample.R
@@ -1,11 +1,11 @@
 #' @export
 runExample <- function() {
-  appDir <- system.file("shiny-examples", "myapp", 
+  appDir <- system.file("shiny-examples", "worldleapp",
                         package = "worldle")
-  if (appDir == "") { 
+  if (appDir == "") {
     stop(paste0("Could not find example directory. ",
                 "Try re-installing `mypackage`."), call. = FALSE)
   }
   # the first app will be called
-  shiny::runApp(appDir[1], display.mode = "normal") 
+  shiny::runApp(appDir[1], display.mode = "normal")
 }
