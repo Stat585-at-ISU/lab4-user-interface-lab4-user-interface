c7440dd3f727318667635eee92514b69f26655f5
valeriehan6 <valerie.han6@gmail.com>
2023-04-13 14:39:29 -0500

    added R/runExample.R

---
+++ R/runExample.R
@@ -0,0 +1,11 @@
+#' @export
+runExample <- function() {
+  appDir <- system.file("shiny-examples", "myapp", 
+                        package = "worldle")
+  if (appDir == "") { 
+    stop(paste0("Could not find example directory. ",
+                "Try re-installing `mypackage`."), call. = FALSE)
+  }
+  # the first app will be called
+  shiny::runApp(appDir[1], display.mode = "normal") 
+}
