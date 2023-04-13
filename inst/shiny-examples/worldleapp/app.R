f29107d0da2ace2b2ec9d410d2799d7ebc208f27
valeriehan6 <valerie.han6@gmail.com>
2023-04-13 15:04:56 -0500

    shiny_start written

--- inst/shiny-examples/worldleapp/app.R
+++ inst/shiny-examples/worldleapp/app.R
@@ -9,6 +9,48 @@
 
 library(shiny)
 
+shiny_start <- function() {
+  countries <- get0("countries", envir = asNamespace("worldle"))
+  
+  today <- lubridate::today()
+  set.seed(as.numeric(today))
+  todays_country_id <- sample(nrow(countries), 1)
+  
+  iso_rda <- countries$iso_rda[todays_country_id]
+  if (!file.exists(file.path("data", iso_rda))) {
+    sf <- get_shapes(file.path("https://geodata.ucdavis.edu/gadm/gadm4.1/shp/",countries$links[todays_country_id]))
+    sf <- thin(sf, 0.001)
+  }
+  
+  gg <- sf %>% ggplot() + geom_sf() + theme_void()
+  
+}
+
+
+# move this somewhere else
+shiny_guess(sf, countries$country.name.en[todays_country_id], attempt=1, guess="", countries$unicode.symbol[todays_country_id])
+
+#' @importFrom dplyr `%>%`
+#' @importFrom ggplot2 ggplot theme_void geom_sf
+#' @importFrom stringr str_equal
+shiny_guess <- function(sf, name, attempt=1, guess="", unicode) {
+  
+  print(gg)
+  if (attempt==1)
+    cat("Which country is it? ")
+  else {
+    cat(sprintf("%d. attempt", attempt))
+  }
+  guess <- scan(what=character(), n=1)
+  if (str_equal(tolower(name), tolower(guess))) {
+    cat(sprintf("\nYou got it in %d attempts, congratulations!", attempt))
+    return()
+  }
+  if (attempt < 6) play(sf, name, attempt+1, guess, unicode)
+  else cat(sprintf("\nSo close! It is %s!", name))
+}
+
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
